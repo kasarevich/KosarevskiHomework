88153f4aa42b6cc5ce9c5235e357117c64f56884
kasarevich <33556532+kasarevich@users.noreply.github.com>
2017-12-07 01:41:32 +0200

    started task2

--- HomeWork/HomeWork8/src/by/it_academy/task2/Main.java
+++ HomeWork/HomeWork8/src/by/it_academy/task2/Main.java
@@ -1,12 +1,5 @@
-package by.it_academy.task1;
-/*       Делаем вот это задание:
-     Написать простой калькулятор в консоли, который будет уметь складывать, отнимать, умножать и делить числа, и выводить результат.
-     Числа вводить в консоли и проверять на корректность + проверять во время операций сложения, вычитания и тд.
-     Выбор оператора (+, -  и т. д.) можно спрашивать у пользователя в виде:
-     - введите 1, чтобы сложить 2 числа
-     - введите 2, чтобы умножить 2 числа
-     - и т. д.
-     Но возможно вы сможете придумать более крутое решение по вводу данных ) это только приветствуется.*/
+package by.it_academy.task2;
+
 
 import java.io.IOException;
 import java.util.InputMismatchException;
@@ -16,9 +9,9 @@ public class Main {
     public static void main(String []args){
         int i = 0;
         while(i == 0){
-        double rez = operation();
-        System.out.println("Результат операции равен " + rez);
-        System.out.println("нажмине 0, чтобы выполнить еще один расчет:");
+            double rez = operation();
+            System.out.println("Результат операции равен " + rez);
+            System.out.println("нажмине 0, чтобы выполнить еще один расчет:");
             Scanner in = new Scanner(System.in);
             i = in.nextInt();
         }
@@ -26,7 +19,7 @@ public class Main {
 
     }
 
-    private static double input()throws IOException, InputMismatchException   { // Вместо запятой могут ввести точку, поэтому проверка на InputMismatchException
+    private static double input()throws IOException, InputMismatchException { // Вместо запятой могут ввести точку, поэтому проверка на InputMismatchException
         Scanner in = new Scanner(System.in);
         System.out.println("Введите число:");
         double num = in.nextDouble();
@@ -46,7 +39,7 @@ public class Main {
         }catch (InputMismatchException e){
             System.out.println("Неверный символ!");
             return 0;
-    }
+        }
         System.out.println("Введите 1 для сложения\n\t\t2 для вычитания\n\t\t3 для умножения\n\t\t4 для деления");
         int flag = io.nextInt();
         double rez = 0;
---
+++ HomeWork/HomeWork8/src/by/it_academy/task2/MyIOException.java
@@ -0,0 +1,7 @@
+package by.it_academy.task2;
+
+public class MyIOException extends Exception{
+    public MyIOException(String description){
+        super(description);
+    }
+}
