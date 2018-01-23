e83b913c501ebd6b3863ccae4dbba3663d477081
kasarevich <33556532+kasarevich@users.noreply.github.com>
2018-01-23 02:04:07 +0200

    mn

---
+++ HomeWork/HomeWork19/src/by/it_academy/App.java
@@ -0,0 +1,14 @@
+package by.it_academy;
+
+
+
+public class App {
+    public static void main(String[] args) {
+
+        MyCollection<String, String> myCollection = new MyCollection();
+        //  for(String staff : myCollection) - сделать с помощью паттерна итератор
+        // имплементим класс Iterator
+
+
+    }
+}
